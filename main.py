d55d4399bb1874db6184d8a89eac31b9b16bfd83
Kritaphat Songsri-in <kritaphat.songsri-in11@imperial.ac.uk>
2016-10-28 16:28:20 +0100

    Add create_response_map

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from menpofit.dpm.main import get_model
 from resnet_feature_pyramid import ResnetFeaturePyramid
 import numpy as np
 import menpo.io as mio
+from menpo.image import Image
+import matplotlib.pyplot as plt
 
 def debugging():
     # im = mio.import_image('/vol/atlas/databases/aflw_ibug/face_52982.jpg', normalise=False)
@@ -17,9 +19,10 @@ def debugging():
     # for i in range(68):
     #     index = np.where(np.array(normal_to_pie)==i)[0][0]
     #     pie_to_normal.append(index)
-    # for i in range(68):
-    #     dpm_learner.train_part_fast('/vol/atlas/homes/ks3811/pickles/resnet/multipie', i)
-    dpm_learner.train_component(pickle_dev, 0)
+    for i in range(68):
+        dpm_learner.train_part_fast('/vol/atlas/homes/ks3811/pickles/resnet/5x5_dpm_filters', i)
+    # dpm_learner.train_component(pickle_dev, 0)
+    # dpm_learner.train_final_component(pickle_dev, 0)
     # pickle_dev = '/vol/atlas/homes/ks3811/pickles/resnet'
     # model_name = 'parts_model_0.pkl'
     # model = get_model(pickle_dev, model_name, resnet_fea_pyramid)
@@ -34,11 +37,11 @@ def train_parts():
         dpm_learner.train_part('/vol/atlas/homes/ks3811/pickles/resnet', i)
 
 def visualize_tree():
-    pickle_dev = '/vol/atlas/homes/ks3811/pickles/resnet/multipie'
+    pickle_dev = '/vol/atlas/homes/ks3811/pickles/resnet/5x5_dpm_filters/pose_4'
     dpm_learner = DPMLearner()
-    defs = dpm_learner.build_defs(pickle_dev, 0, 0)
+    defs = dpm_learner.build_defs(pickle_dev, 0, 1)
     points = np.zeros((68, 2))
-    tree = dpm_learner.get_aflw_tree(0)
+    tree = dpm_learner.get_aflw_tree(1)
     for depth in range(1, tree.maximum_depth + 1):
         for cv in tree.vertices_at_depth(depth):  # for each vertex in that level
             par = tree.parent(cv)
@@ -49,14 +52,74 @@ def visualize_tree():
     return points
 
 def test_learn_model():
-    pickle_dev = '/vol/atlas/homes/ks3811/pickles/resnet'
-    model_name = 'parts_model_0.pkl'
-    resnet_fea_pyramid = ResnetFeaturePyramid()
-    model = get_model(pickle_dev, model_name, resnet_fea_pyramid)
-    print(model)
+    pickle_dev = '/vol/atlas/homes/ks3811/pickles/resnet/multipie'
+    model_name = '10pos_component_0.pkl'
+    resnet = ResnetFeaturePyramid()
+    model = get_model(pickle_dev, model_name, resnet)
+    import time
+    for i in range(5):
+        image = mio.import_builtin_asset('takeo.ppm', normalize=False)
+        image = image.crop_to_landmarks_proportion(np.random.rand())
+        start = time.time()
+        boxes = DPMFitter.fast_fit_from_model(image, model, 0, return_once=True)
+        stop = time.time()
+        print('fitting tak:', stop-start)
+        print('---------------------')
+
+def create_response_map():
+    images = mio.import_images('/vol/atlas/homes/jiankang/COFW_test', normalize=False)
+    resnet = ResnetFeaturePyramid()
+    for image in images:
+        fea = resnet.extract_feature(image)
+        cmap = plt.get_cmap('jet')
+        sum_fea = np.sum(fea, axis=0) - fea[0, :, :]
+        rgba_img = cmap(sum_fea)
+        rgb_img = np.delete(rgba_img, 3, 2)
+        rgb_img = np.transpose(rgb_img, [2, 0, 1])
+        # response = response/np.max(response)
+        # response = response/255.0
+        print(rgba_img.shape)
+        print(rgb_img.shape)
+        response = Image(rgb_img)
+        print('/vol/atlas/homes/jiankang/COFW_test/' + str(image.path).split('/')[-1].split('.')[0] + '_result.png')
+        mio.export_image(response, '/vol/atlas/homes/ks3811/public/COFW_test/' + str(image.path).split('/')[-1].split('.')[0] + '_result.png', overwrite=True)
+    print(images)
+
+def find_hard_negatives():
+    pickle_dev = '/vol/atlas/homes/ks3811/pickles/resnet/5x5_dpm_filters'
+    resnet = ResnetFeaturePyramid()
+    dpm_learner = DPMLearner(feature_pyramid=resnet)
+    model = dpm_learner.build_model(pickle_dev, 0)
+    filters, defs2, defs, bias = mio.import_pickle('/vol/atlas/homes/ks3811/pickles/fast_dpm/5x5_dpm_filters.pkl')
+    model.update_model_from_pickle(filters, defs2, defs, bias)
+
+    hard_negatives = []
+    _, negs = DPMLearner._get_frontal_pie_image_info(pickle_dev)
+    for neg in negs:
+        image = mio.import_image(neg['im'], normalize=False)
+        boxes = DPMFitter.fast_fit_from_model(image, model, -2)
+        print(boxes)
+        if len(boxes) > 0:
+            max_score = np.max(np.array([box['s'] for box in boxes]))
+            hard_negatives.append({'neg': neg, 'max_score': max_score, 'boxes': boxes})
+            if max_score >= -1:
+                print(neg['im'], max_score)
+    mio.export_pickle(hard_negatives, '/vol/atlas/homes/ks3811/pickles/resnet/5x5_dpm_filters/hard_negatives.pkl')
+    print(len(hard_negatives))
+
+
+def build_init_model():
+    resnet = ResnetFeaturePyramid()
+    # model = get_model(pickle_dev, model_name, resnet)
+    pickle_dev = '/vol/atlas/homes/ks3811/pickles/resnet/5x5_dpm_filters/pose_1'
+    learner = DPMLearner(feature_pyramid=resnet)
+    model = learner.build_model(pickle_dev, 1)
+    pass
 
 if __name__ == "__main__":
     # sys.exit(visualize_tree())
     # sys.exit(train_parts())
-    sys.exit(debugging())
+    # sys.exit(find_hard_negatives())
     # sys.exit(test_learn_model())
+    # sys.exit(build_init_model())
+    sys.exit(create_response_map())
--- resnet_feature_pyramid.py
+++ resnet_feature_pyramid.py
@@ -6,8 +6,9 @@ import numpy as np
 
 slim = tf.contrib.slim
 
+
 class ResnetFeaturePyramid(FeaturePyramid):
-    def __init__(self, vgg=None):
+    def __init__(self):
         self.feature_size = 69
         self.spacial_scale = 1
         self.shift = None
@@ -16,6 +17,7 @@ class ResnetFeaturePyramid(FeaturePyramid):
         self.sess = tf.Session()
         with tf.variable_scope('net'):
             self.menpo_image = tf.placeholder(tf.float32, shape=(3, None, None))
+            self.menpo_original_shape = tf.placeholder(tf.int32, shape=(2))
             with slim.arg_scope([slim.batch_norm, slim.layers.dropout], is_training=False):
                 image = tf.expand_dims(tf.transpose(self.menpo_image, [1, 2, 0]), 0)
                 prediction, _ = resnet_model.multiscale_kpts_net(image, scales=(1, 2, 4))
@@ -25,32 +27,35 @@ class ResnetFeaturePyramid(FeaturePyramid):
         saver.restore(self.sess, model_path)
         self.softmax = tf.nn.softmax(prediction)
 
+        feature = tf.image.resize_images(self.softmax, self.menpo_original_shape)
+        self.resized_softmax = tf.transpose(tf.squeeze(feature, [0]), [2, 0, 1])
 
     def extract_feature(self, img):
         original_shape = img.shape
         if img.n_channels == 1:
             img.pixels = np.vstack([img.pixels]*3)
-        import time
-        start = time.time()
+        # import time
+        # start = time.time()
         img = rescale_image(img)
         img_pixels = caffe_preprocess(img)
-        stop = time.time()
-        print('preprocess take:', stop-start)
-        feature = tf.image.resize_images(self.softmax, (tf.to_int32(original_shape[0]), tf.to_int32(original_shape[1])))
-        transpose_feature = tf.transpose(tf.squeeze(feature, [0]), [2, 0, 1])
-        result = self.sess.run(transpose_feature, feed_dict={self.menpo_image: img_pixels})
-        start = time.time()
-        print('extracting feature take:', start-stop)
+        # stop = time.time()
+        # print('preprocess take:', stop-start)
+        result = self.sess.run(
+                self.resized_softmax,
+                feed_dict={
+                    self.menpo_image: img_pixels,
+                    self.menpo_original_shape: np.array(original_shape, np.int32)
+        })
+        # start = time.time()
+        # print('extracting feature take:', start-stop)
         return result
 
     def extract_pyramid(self, img, interval=0, pyramid_pad=(0, 0)):
-        spacial_scale = self.spacial_scale
-        shift = self.shift
         fea = self.extract_feature(img)
-        import time
-        start = time.time()
+        # import time
+        # start = time.time()
         fea = np.pad(fea, ((0, 0), (int(pyramid_pad[1]), int(pyramid_pad[1])),
                                                  (int(pyramid_pad[0]), int(pyramid_pad[0]))), 'constant')
-        stop = time.time()
-        print('padding take:', stop-start)
+        # stop = time.time()
+        # print('padding take:', stop-start)
         return {0:fea}, {0:self.spacial_scale}, self.shift
